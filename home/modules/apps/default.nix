ff282eda376963c32eaf3cbf859bb0fc17283232
Pfeiffer Gabor <gaborpfeiffer221@gmail.com>
2026-04-07 21:16:22 +0200

    add firefox and some programing stuff

--- home/modules/apps/default.nix
+++ home/modules/apps/default.nix
@@ -7,6 +7,7 @@
 
       # desktop
       google-chrome
+      firefox
       opencode
   ];
 }
--- home/modules/programing/default.nix
+++ home/modules/programing/default.nix
@@ -3,6 +3,9 @@
   home.packages = with pkgs; [
     php
     phpPackages.php-cs-fixer
+    phpPackages.composer
     phpstan
+    nodejs
+    pnpm
   ];
 }
